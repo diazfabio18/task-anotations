e3233d2a8a84125506c2b58a2e4ff5036db0bb00
diazfabio18 <fabiodiaz@est.frba.utn.edu.ar>
2020-09-11 20:20:02 -0300

    Updates

---
+++ precache-manifest.325ba919f62227ca0de83e6b55d2beec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd993c7e8127d6e57bf36471a40692a",
+    "url": "/task-anotations/index.html"
+  },
+  {
+    "revision": "591f1aa85ba8233e31bf",
+    "url": "/task-anotations/static/css/2.acc62604.chunk.css"
+  },
+  {
+    "revision": "b74cc0045ad082260141",
+    "url": "/task-anotations/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "591f1aa85ba8233e31bf",
+    "url": "/task-anotations/static/js/2.d1fb90c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-anotations/static/js/2.d1fb90c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b74cc0045ad082260141",
+    "url": "/task-anotations/static/js/main.525915f5.chunk.js"
+  },
+  {
+    "revision": "b97063888051c63e3ed9",
+    "url": "/task-anotations/static/js/runtime-main.4d9009d6.js"
+  }
+]);
